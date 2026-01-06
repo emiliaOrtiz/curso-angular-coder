d826c30a4a5a3032a8205a581c026fa673f25b0f
emilia <rikukim@gmail.com>
2026-01-06 20:27:23 +0100

    modificatio Estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink,RouterLinkActive} from '@angular/router';
 import { HomeComponent } from './home.component';
 
 //quiero agregar un router-outlet para que funcione la navegacion
@@ -8,15 +8,21 @@ import { HomeComponent } from './home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HomeComponent, RouterOutlet],
+  imports: [CommonModule, HomeComponent, RouterOutlet,RouterLink,RouterLinkActive],
   template: `
   <div style="background: pink; padding: 10px; text-align: center; font-weight: bold; color: #5D4037;">
-    ¡We are live! ✨
+    ¡We are live - stay tuned for new collections arrived soon! ✨
   </div>
 
   <router-outlet></router-outlet>
-`
+`,
+styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // Additional logic can be added here if needed
+  
+    isScrolled = false;
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+      this.isScrolled = window.scrollY > 50;
+    } // Additional logic can be added here if needed
 }
